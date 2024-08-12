ff3c0700e4343d4f2d6de8c1881f265f019bf3dc
Jimbowei <1662056@qq.com>
2024-08-12 22:20:35 +0800

    Site updated: 2024-08-12 22:20:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/11/hello-world/","2024/08/12/tips-and-tricks/"];function toRandomPost(){
+var posts=["2024/08/12/tips-and-tricks/","2024/08/11/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
