c1ba80b11ce8ceba590196bb6eb509b1d46d975a
Jimbowei <1662056@qq.com>
2024-08-14 21:27:41 +0800

    Site updated: 2024-08-14 21:27:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/12/tips-and-tricks/","2024/08/11/hello-world/"];function toRandomPost(){
+var posts=["2024/08/13/markdown-test/","2024/08/11/hello-world/","2024/08/12/tips-and-tricks/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
